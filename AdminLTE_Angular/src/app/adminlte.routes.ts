c7f93d38724507bc5961e2a9aa4176972b1d879e
Fernando Ramirez <Fernando.ramirez.sarmiento@gmail.com>
2021-02-15 09:01:49 -0300

    Actualizacion AdmiLTE

--- AdminLTE_Angular/src/app/adminlte.routes.ts
+++
@@ -1,9 +0,0 @@
-import { RouterModule, Routes } from '@angular/router';
-import {BlankComponent} from './././components/body/blank/blank.component';
-
-const app_routes: Routes = [
-  { path: 'home', component: BlankComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
-];
-
-export const app_routing = RouterModule.forRoot(app_routes);
